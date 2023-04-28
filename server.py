e602ba16b93e270bb32b88c24b679ed2a62d31d3
Bobbins228 <markcampbell114@gmail.com>
2023-04-28 12:28:13 +0100

    Changes to connection message and README

--- server.py
+++ server.py
@@ -19,7 +19,11 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    #socketio
-    print('connect at : http://localhost:8080')
+    #Multiple clients can connect to either localhost at port 8080 or the machine's local_ip:8080
+    print('connect at : http://localhost:8080 or http://local_ip:8080')
     serve(app=app, host='0.0.0.0', port=8080, url_scheme='HTTP', threads=6)
+    """
+    This is the old way of connecting to the server that didn't support multiple clients
+    Eventlet also printed a trace error whenever the client would disconnect but serve does not
+    """
     #eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), appServer)
